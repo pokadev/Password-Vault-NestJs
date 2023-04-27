5fbd32891856054846f3f2332d2164bd58e44300
pokaahaha <men101@protonmail.com>
2023-04-27 10:24:35 +0300

    add pino(logger)v7

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,17 @@
 import createServer from './utils/createServer';
+import logger from './utils/logger';
+
+
+
 async function main(){
 const app = createServer()
 
 try{
   const url = await app.listen(4000, '0.0.0.0')
-  console.log(`Server listening on ${url}`)
+  logger.info(`Server listening on ${url}`)
 }
 catch(err){
-  console.error(err)
+  logger.error(err)
   process.exit(1)
 }
 }
