4eb3f8b1621207acc64c07b06546ab5a1fea356d
pokaahaha <men101@protonmail.com>
2023-04-27 19:07:48 +0300

    ADD: client register form

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -1,8 +1,15 @@
+import { FormControl, FormLabel, Heading, Input } from "@chakra-ui/react";
+import FormWrapper from "./FormWrapper";
 
 function RegisterForm() {
-  return (
-    <div>RegisterForm</div>
-  )
+  return;
+  <FormWrapper>
+    <Heading>Register</Heading>
+    <FormControl mt="4">
+      <FormLabel htmlFor="email">Email</FormLabel>
+      <Input id="email" placeholder="Email" />
+    </FormControl>
+  </FormWrapper>;
 }
 
-export default RegisterForm
+export default RegisterForm;
