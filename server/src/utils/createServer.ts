fb97e8f95c63e6293dd0237306821cbc7da6f2de
pokaahaha <men101@protonmail.com>
2023-04-27 11:11:54 +0300

    add: cors

--- server/src/utils/createServer.ts
+++ server/src/utils/createServer.ts
@@ -1,7 +1,16 @@
 import fastify from 'fastify';
-
+import cors from '@fastify/cors';
+import { CORS_ORIGIN } from '../constants';
 function createServer () {
   const app = fastify();
+
+  app.register(cors,{
+    origin: CORS_ORIGIN,
+    credentials: true
+  })
+
+  
+
   return app;
 }
 
