56b12f7eab378ce705ebd73f3aac41475bfa1d99
pokaahaha <men101@protonmail.com>
2023-04-28 11:07:59 +0300

    ADD: login api

--- server/src/modules/user/user.controller.ts
+++ server/src/modules/user/user.controller.ts
@@ -1,8 +1,13 @@
-import { FastifyReply, FastifyRequest } from "fastify";
+import { FastifyReply } from "fastify";
+import { FastifyRequest } from "fastify";
 import { COOKIE_DOMAIN } from "../../constants";
 import logger from "../../utils/logger";
-import { createVault } from "../vault/vault.service";
-import { createUser, generateSalt } from "./user.service";
+import { createVault, findVaultByUser } from "../vault/vault.service";
+import {
+  createUser,
+  findUserByEmailAndPassword,
+  generateSalt,
+} from "./user.service";
 
 export async function registerUserHandler(
   request: FastifyRequest<{
@@ -37,4 +42,38 @@ export async function registerUserHandler(
     logger.error(e, "error creating user");
     return reply.code(500).send(e);
   }
+}
+
+export async function loginHandler(
+  request: FastifyRequest<{
+    Body: Parameters<typeof createUser>[number];
+  }>,
+  reply: FastifyReply
+) {
+  const user = await findUserByEmailAndPassword(request.body);
+
+  if (!user) {
+    return reply.status(401).send({
+      message: "Invalid email or password",
+    });
+  }
+
+  const vault = await findVaultByUser(user._id);
+
+  const accessToken = await reply.jwtSign({
+    _id: user._id,
+    email: user.email,
+  });
+
+  reply.setCookie("token", accessToken, {
+    domain: COOKIE_DOMAIN,
+    path: "/",
+    secure: false,
+    httpOnly: true,
+    sameSite: false,
+  });
+
+  return reply
+    .code(200)
+    .send({ accessToken, vault: vault?.data, salt: vault?.salt });
 }
--- server/src/modules/user/user.route.ts
+++ server/src/modules/user/user.route.ts
@@ -1,5 +1,5 @@
 import { FastifyError, FastifyInstance, FastifyPluginOptions } from "fastify";
-import { registerUserHandler,  } from './user.controller';
+import { loginHandler, registerUserHandler } from "./user.controller";
 
 function userRoutes(
   app: FastifyInstance,
@@ -8,6 +8,7 @@ function userRoutes(
 ) {
   app.post("/", registerUserHandler);
 
+  app.post("/login", loginHandler);
 
   done();
 }
